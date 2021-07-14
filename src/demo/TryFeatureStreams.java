8ae8d75bba39c4628cf0ed86c3a749859e2b2ac4
christoferson <sweetsorrow1981@yahoo.co.jp>
2021-07-14 23:46:46 +0900

    Add Examples

--- src/demo/TryFeatureStreams.java
+++ src/demo/TryFeatureStreams.java
@@ -7,14 +7,44 @@ public class TryFeatureStreams {
 	public static void main(String[] args) {
 
 		tryBasic();
+		
+		tryFilter();
+		
+		tryMap();
+		
+		tryPeak();
 	}
 	
 	private static void tryBasic() {
 		Arrays.asList("029", "52", "913", "472").stream()
-			.filter(e -> { return Integer.parseInt(e) < 500; })
-			.map(e -> {return String.format("(%s)", e);})
+			.filter(e -> Integer.parseInt(e) < 500 )
+			.map(e -> String.format("(%s)", e))
 			.forEach(System.out::print);
-			
+		System.out.println();
+	}
+	
+	private static void tryFilter() {
+		
+		Arrays.asList("Warrior", "Rogue", "Priest", "Wizard", "Druid").stream()
+			.filter(e -> e.startsWith("W"))
+			.forEach(System.out::print);
+		System.out.println();
+	}
+	
+	private static void tryMap() {
+		
+		Arrays.asList("Warrior", "Rogue", "Priest", "Wizard", "Druid").stream()
+			.map(String::toUpperCase)
+			.forEach(System.out::print);
+		System.out.println();
+	}
+	
+	private static void tryPeak() {
+		
+		Arrays.asList("Warrior", "Rogue", "Priest", "Wizard", "Druid").stream()
+			.peek(System.out::print)
+			.forEach(System.out::print);
+		System.out.println();
 	}
 
 }
