5905ad745bbc3f17d1c7ce5c6f89766b4c6b4ba5
christoferson <sweetsorrow1981@yahoo.co.jp>
2021-10-03 01:02:20 +0900

    Add tryCollectorsFiltering

--- src/demo/TryFeatureStreamJava9.java
+++ src/demo/TryFeatureStreamJava9.java
@@ -1,26 +1,37 @@
 package demo;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class TryFeatureStreamJava9 {
 
 	public static void main(String[] args) {
 		List<Runnable> list = new ArrayList<Runnable>();
-		list.add(TryFeatureStreamJava9::tryBasic);
+		list.add(TryFeatureStreamJava9::tryCollectorsFiltering);
 
 		for (var r : list) {
 			r.run();
 		}
 	}
 	
-	private static void tryBasic() {
-		Arrays.asList("029", "52", "913", "472").stream()
-			.filter(e -> Integer.parseInt(e) < 500 )
-			.map(e -> String.format("(%s)", e))
-			.forEach(System.out::print);
-		System.out.println();
+	private static void tryCollectorsFiltering() {
+
+	    List<Integer> numbers = List.of(1, 2, 3, 5, 5, 7);
+
+	    Map<Integer, Long> result = numbers.stream()
+	      .filter(val -> val > 3)
+	      .collect(Collectors.groupingBy(i -> i, Collectors.counting()));
+
+	    System.out.println(result);
+
+	    result = numbers.stream()
+	      .collect(Collectors.groupingBy(i -> i,
+	        Collectors.filtering(val -> val > 3, Collectors.counting())));
+
+	    System.out.println(result);
+	    
 	}
 
 }
